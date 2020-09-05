bbf35b783e3bc40707459fe6c85f426389b1735e
OscarOosterling <oscar-oosterling@live.nl>
2020-09-05 07:45:05 +0200

    Completed step 5.1, had some problems because i changed class name without refactoring it

--- app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
+++ app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
@@ -2,10 +2,26 @@ package com.example.madlevel1task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.madlevel1task1.databinding.ActivityHigherLowerBinding
 
-class MainActivity : AppCompatActivity() {
+class HigherLowerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHigherLowerBinding
+    private var currentThrow: Int = 1
+    private var lastThrow: Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_higher_lower)
+        binding = ActivityHigherLowerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews() {
+        updateUI()
+    }
+
+    private fun updateUI() {
+        binding.tvLastThrow.text = getString(R.string.last_throw, lastThrow)
     }
 }
